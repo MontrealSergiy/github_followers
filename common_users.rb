c270cdb95c9d048b3fd136bae0397a855f3dcac9
USERUNDETECTED <98942155+USERUNDETECTED@users.noreply.github.com>
2022-02-04 07:18:37 -0500

    Create common_users.rb

---
+++ common_users.rb
@@ -0,0 +1,122 @@
+#!/usr/bin/env ruby
+
+# (c) USERUNDETECTED 2022
+# a githup api cli utility
+
+
+require "octokit"
+require "set"
+
+
+AUTH_TOKEN = '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'
+PER_PAGE = 100 # maximize the page
+TRIES = 3 # retry to counter network/rate issues
+
+
+@options  = {}
+usernames = []
+@client = Octokit::Client.new(:access_token => AUTH_TOKEN)
+
+
+# easy way is to switch on autopaging and retireve followers in one shot
+# but I will try to optimize a little
+
+#check that users exist
+def user_check(usernames)
+  usernames.each do |name|
+    begin
+      @client.user name
+        # normally I'm fine with program to spit the exception in the face,
+        # yet exception handling was requested
+    rescue Octokit::NotFound => e
+      STDERR.puts "ERROR, USER #{name} DOES NOT EXIST \n\n Details #{e.message}"
+      exit 1
+    rescue Octokit::Unauthorized => e
+      STDERR.puts "ERROR, UPDATE THE CREDENTIALS \n\n Details #{e.message}"
+      exit 1
+    rescue => e
+      STDERR.puts "TERMINAL ERROR \n\n Details #{e.message}"
+      exit 1
+    end
+  end
+end
+
+#pulls one page of followers
+def get_page(user, page, tries)
+  begin
+    @client.followers(user, page: page, per_page: PER_PAGE).map(&:login)
+  rescue => e
+    if tries > 1
+      STDERR.puts "ERROR, WILL TRY AGAIN AFTER A DELAY \n\n #{e.message}"
+      sleep(1)
+      get_page(user, page, tries - 1)
+    else
+      STDERR.puts "ERROR, TERMINATING \n\n #{e.message}"
+    end
+  end
+end
+
+#finds all followers
+def get_followers(user)
+  page   = 1
+  result = [].to_set
+  until (d = get_page(user, page, TRIES)).empty? do
+    if @options[:progress]
+      puts "step #{page} retrieved next #{PER_PAGE} follower of user #{user}"
+      puts d[-1]
+    end
+    page += 1
+    result.merge(d.to_set)
+  end
+  result
+end
+
+# finds common followers
+def common_followers(user1, user2)
+  if @options[:multithreaded]
+    t           = Thread.new { @followers2 = get_followers(user2) }
+    @followers1 = get_followers(user1)
+    t.join()
+    @followers1.intersection @followers2
+  else
+    get_followers(user1).intersection get_followers(user2)
+  end
+end
+
+if __FILE__ == $0
+
+  # CLI parsing
+  ARGV.each do |option|
+    case option
+    when "-h"
+      puts "The command line utility finds common followers for two github users."
+      puts "Syntax is"
+      puts "  common_followers.rb username1 username2 [-p] [-m]"
+      puts "-p : show the progress"
+      puts "-m : multithreaded mode"
+      exit
+    when "-p"
+      @options[:progress] = true
+    when "-m"
+      @options[:multithreaded] = true
+    else
+      usernames.push(option)
+    end
+  end
+  if usernames.length != 2
+    puts "Error. The command line utility finds common followers for two github users."
+    puts "Syntax is"
+    puts "  common_followers.rb username1 username2 [-p] [-m]"
+    exit 1
+  end
+
+  result = common_followers(usernames[0], usernames[1])
+  puts 'COMMON FOLLOWERS'
+  puts result.to_a.join("\n")
+
+end
+
+
+# well followed users
+# user1 = 'egoist'
+# user2 = 'andrew'
